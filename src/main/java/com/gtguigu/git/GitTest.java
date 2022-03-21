a9ca1c2cbab6b1e58993bd3fd11b2a9bb114c030
yabax <2407891168@qq.com>
2022-03-21 23:20:43 +0800

    first commit

---
+++ src/main/java/com/gtguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gtguigu.git;
+
+/**
+ * @author yabax
+ * @create 2022-03-21 23:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
